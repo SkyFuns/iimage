9fc21e91276e602ace01cb1d2ddff11e8e790ffd
liangyl <634689854@qq.com>
2018-10-14 22:20:50 +0800

    sada

--- src/Facades/IImage.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Nomandia\IImage\Facades;
-use Illuminate\Support\Facades\Facade;
-
-class IImage extends Facade {
-	protected static function getFacadeAccessor() {
-		return 'iimage';
-	}
-}
